f2c50865e993cd1e10ff6cba606539d27a73008f
Reinanda Faris <142996582+Reinandafaris@users.noreply.github.com>
2024-03-07 22:19:17 +0700

    first commit

--- server/index.js
+++ server/index.js
@@ -1,1 +1,25 @@
-console.log("Implement servermu disini yak 😝!");
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const url = require("url");
+
+const PUBLIC_DIR = path.join(
+  __dirname,
+  "../public"
+);
+const PORT = 8000;
+
+const server = (req, res) => {
+  if (req.url === "/") {
+    res.end("Hello World!");
+  } else if (req.url === "/search") {
+    res.end("Hello Search!");
+  } else {
+    res.end("404 Not Found");
+  }
+};
+
+http.createServer(server).listen(PORT);
+console.log(
+  `Server listening on port http://localhost:${PORT}`
+);
