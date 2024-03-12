df5f0811b64dd086140690dfafc1d68e1af77443
Reinanda Faris <142996582+Reinandafaris@users.noreply.github.com>
2024-03-12 14:30:48 +0700

    feat: filter mobil

--- server/index.js
+++ server/index.js
@@ -2,24 +2,51 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 const url = require("url");
-
-const PUBLIC_DIR = path.join(
+const PUBLIC_DIRECTORY = path.join(
   __dirname,
   "../public"
 );
-const PORT = 8000;
+const PORT = 8080;
 
 const server = (req, res) => {
   if (req.url === "/") {
-    res.end("Hello World!");
-  } else if (req.url === "/search") {
-    res.end("Hello Search!");
+    req.url = "/index.html";
+  } else if (req.url === "/cars") {
+    req.url = "/cars.html";
   } else {
-    res.end("404 Not Found");
+    req.url = req.url;
   }
+  const parseURL = url.parse(req.url);
+  const pathName = `${parseURL.pathname}`;
+  const extension = path.parse(pathName).ext;
+  const absolutePath = path.join(
+    PUBLIC_DIRECTORY,
+    pathName
+  );
+
+  const contentTypes = {
+    ".css": "text/css",
+    ".png": "image/png",
+    ".svg": "image/svg+xml",
+    ".html": "text/html",
+    ".js": "text/javascript",
+  };
+
+  fs.readFile(absolutePath, (err, data) => {
+    if (err) {
+      res.statusCode = 500;
+      res.end("File not found ...");
+    } else {
+      res.setHeader(
+        "Content-Type",
+        contentTypes[extension] || "text/plain"
+      );
+      res.end(data);
+    }
+  });
 };
 
 http.createServer(server).listen(PORT);
 console.log(
-  `Server listening on port http://localhost:${PORT}`
+  `Server is running ... PORT : http://localhost:${PORT}`
 );
